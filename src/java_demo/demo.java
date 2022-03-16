d95209fa7f25c13f8ce08265537bfd7b62934299
Kritik-deloitte <Kribansal@deloitte.com>
2022-03-16 10:22:13 +0530

    Update Commit

---
+++ src/java_demo/demo.java
@@ -0,0 +1,8 @@
+package java_demo;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello main");
+        System.out.println("HII");
+    }
+}
---
+++ src/java_demo/hello.java
@@ -0,0 +1,9 @@
+package java_demo;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello main");
+        System.out.println("HII");
+    }
+
+}
