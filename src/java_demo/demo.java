6bbbe48cfacd08dba086fe28a50feb5027980577
Kritik-deloitte <Kribansal@deloitte.com>
2022-03-19 19:26:55 +0530

    Delete src/java_demo directory

--- src/java_demo/demo.java
+++
@@ -1,9 +0,0 @@
-package java_demo;
-
-public class demo {
-    public static void main(String[] args) {
-        System.out.println("hello main");
-        System.out.println("HII");
-        System.out.println("HII");
-    }
-}
